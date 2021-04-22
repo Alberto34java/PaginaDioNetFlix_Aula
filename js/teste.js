d146448b8c55dbef40988184fc21522ca11f18a6
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-22 17:25:23 -0300

    Testando arquivo js dentro da pagina

--- js/teste.js
+++ js/teste.js
@@ -1,8 +1,14 @@
 function carregarDocumentoEmIngles(){
-    document.getElementById("content").innerHTML='<object type="type/html" data="page.html" ></object>';
+    document.getElementById("elemento").innerHTML='<object type="type/html" data="page.html" ></object>';
 }
 
 
 function load_home() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
+    document.getElementById("elemento").innerHTML='<object type="text/html" data="home.html" ></object>';
 }
+
+$(document).ready( function() {
+    $("#load_home").on("change", function() {
+        $("#elemento").load("page.html");
+    });
+});
