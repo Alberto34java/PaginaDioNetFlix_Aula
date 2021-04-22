b739b7941f79581b3d809a86928ba34942de7a04
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-22 17:13:48 -0300

    Pagina de Iteracao

---
+++ js/teste.js
@@ -0,0 +1,8 @@
+function carregarDocumentoEmIngles(){
+    document.getElementById("content").innerHTML='<object type="type/html" data="page.html" ></object>';
+}
+
+
+function load_home() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
+}
